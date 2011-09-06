d8d891f12d718cb938e65a6051a1b0af6db69b40
Elliot Robinson <elliot.robinson@argiopetech.com>
2011-09-06 16:21:26 -0400

    Additions

---
+++ parseGSV.c
@@ -0,0 +1,321 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "parse.h"
+
+// Carole Fountain  //
+
+/////////////
+//		   //
+//  TO DO  //
+//		   //
+/////////////
+
+
+int parseGSV(struct NMEAData *dataStore, char* sentence) {
+    char* token;
+    short prn = 0;
+    short snr = 0;
+    short azimuth = 0;
+    short elevation = 0;
+
+    //////////////////////////////////////////////////
+    //				                                //
+    //  Extract Number of sentences for full data   //
+    //				                                //
+    //////////////////////////////////////////////////
+	
+    tokenize(token, tokenize(sentence, ","));
+	
+    // We don't need this value so moving on...
+	
+	
+    //////////////////////////////////////
+    //					                //
+    //  Extract what sentence (1 or 2)  //
+    //					                //
+    //////////////////////////////////////
+	
+    // extract what sentence (1 or 2)
+    tokenize(token, tokenize(sentence, ","));
+ 
+    // We don't care so moving on...
+    
+    ////////////////////////////////////////////
+    //					                      //
+    //  Extract number of satellites in view  //
+    //					                      //
+    ////////////////////////////////////////////
+	
+    // extract # of satellites
+    tokenize(token, tokenize(sentence, ","));
+	
+    // We already have this info so moving on...
+	
+    /**** FOR THE FOLLOWING PARAMETERS, INFORMATION CAN BE GIVEN FOR 
+	  UP TO 4 SATELLITES PER SENTENCE  ***/
+	
+    //////////////////////////
+    //					    //
+    //  Extract PRN number  //
+    //					    //
+    //////////////////////////
+	
+    // turn PRN (for 1st satellite) into a useful value
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        // convert to int
+	prn = (short)strtol(token, NULL, 10);
+        dataStore->satellites[0].prn = prn; // store value in prearranged spot
+    }
+	
+    /////////////////////////
+    //					   //
+    //  Extract Elevation  //
+    //				   	   //
+    /////////////////////////
+	
+    // extract elevation
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	elevation = (short)strtol(token, NULL, 10);
+	dataStore->satellites[0].elevation = elevation;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract Azimuth   //
+    //					  //
+    ////////////////////////
+	
+    // extract azimuth
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        //convert to int
+        azimuth = (short)strtol(token, NULL, 10);
+        dataStore->satellites[0].azimuth = azimuth;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract SNR       //
+    //					  //
+    ////////////////////////
+	
+    // extract SNR
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	snr = (short)strtol(token, NULL, 10);
+	dataStore->satellites[0].snr = snr;
+    }
+	
+    //////////////////////////
+    //					    //
+    //  Extract PRN number  //
+    //					    //
+    //////////////////////////
+	
+    // turn PRN (for 2nd satellite) into a useful value
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        // convert to int
+        prn = (short)strtol(token, NULL, 10);
+        dataStore->satellites[1].prn = prn; // store value in prearranged spot
+    }
+	
+    /////////////////////////
+    //					   //
+    //  Extract Elevation  //
+    //				   	   //
+    /////////////////////////
+	
+    // extract elevation
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	elevation = (short)strtol(token, NULL, 10);
+	dataStore->satellites[1].elevation = elevation;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract Azimuth   //
+    //					  //
+    ////////////////////////
+	
+    // extract azimuth
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        //convert to int
+        azimuth = (short)strtol(token, NULL, 10);
+        dataStore->satellites[1].azimuth = azimuth;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract SNR       //
+    //					  //
+    ////////////////////////
+	
+    // extract SNR
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	snr = (short)strtol(token, NULL, 10);
+	dataStore->satellites[1].snr = snr;
+    }
+	
+    //////////////////////////
+    //					    //
+    //  Extract PRN number  //
+    //					    //
+    //////////////////////////	
+    // turn PRN (for 3rd satellite) into a useful value
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        // convert to int
+        prn = (short)strtol(token, NULL, 10);
+        dataStore->satellites[2].prn = prn; // store value in prearranged spot
+    }
+	
+    /////////////////////////
+    //					   //
+    //  Extract Elevation  //
+    //				   	   //
+    /////////////////////////
+	
+    // extract elevation
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	elevation = (short)strtol(token, NULL, 10);
+	dataStore->satellites[2].elevation = elevation;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract Azimuth   //
+    //					  //
+    ////////////////////////
+	
+    // extract azimuth
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        //convert to int
+        azimuth = (short)strtol(token, NULL, 10);
+        dataStore->satellites[2].azimuth = azimuth;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract SNR       //
+    //					  //
+    ////////////////////////
+	
+    // extract SNR
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	snr = (short)strtol(token, NULL, 10);
+	dataStore->satellites[2].snr = snr;
+    }
+	
+    //////////////////////////
+    //					    //
+    //  Extract PRN number  //
+    //					    //
+    //////////////////////////
+	
+    // turn PRN (for 4th satellite) into a useful value
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        // convert to int
+        prn = (short)strtol(token, NULL, 10);
+        dataStore->satellites[3].prn = prn; // store value in prearranged spot
+    }
+	
+    /////////////////////////
+    //					   //
+    //  Extract Elevation  //
+    //				   	   //
+    /////////////////////////
+	
+    // extract elevation
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	elevation = (short)strtol(token, NULL, 10);
+	dataStore->satellites[3].elevation = elevation;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract Azimuth   //
+    //					  //
+    ////////////////////////
+	
+    // extract azimuth
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY)
+    {
+        //convert to int
+        azimuth = (short)strtol(token, NULL, 10);
+        dataStore->satellites[3].azimuth = azimuth;
+    }
+	
+    ////////////////////////
+    //					  //
+    //  Extract SNR       //
+    //					  //
+    ////////////////////////
+	
+    // extract SNR
+    tokenize(token, tokenize(sentence, ","));
+	
+    if (NONEMPTY) 
+    {
+	// convert to int
+	snr = (short)strtol(token, NULL, 10);
+	dataStore->satellites[3].snr = snr;
+    }
+	
+	
+    return 0;
+}
+
+
+
+
+
+
+
---
+++ parseRMC.c
@@ -0,0 +1,136 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "parse.h"
+
+// Carole Fountain //
+
+int parseRMC(struct NMEAData *dataStore, char* sentence) {
+    char* token;
+    short utcTime = 0;
+    short lat = 0;
+    short lon = 0;
+
+    //////////////////////////////
+    //				            //
+    //  Extract UTC time        //
+    //				            //
+    //////////////////////////////
+	
+    // get UTC time 
+    tokenize(token, sentence, ",");
+    
+    if (NONEMPTY)
+    {
+        dataStore->utcTime;
+    }
+	
+	
+    //////////////////////
+    //			        //
+    //  Extract Status  //
+    //			        //
+    //////////////////////
+	
+    // extract status
+    tokenize(token, sentence, ",");
+ 
+    // Don't care move on...
+    
+    ////////////////////////
+    //                    //
+    //  Extract Latitude  //
+    //			          //
+    ////////////////////////
+	
+    // extract latitude
+    tokenize(token, sentence, ",");
+	
+    if (NONEMPTY) {
+	// convert latitude to float
+	lat = strtof(token, NULL);
+		
+	// extract N/S val
+	tokenize(token, sentence, ",");
+		
+	// if South, make latitude negative
+	if (toupper(token) == 'S') 
+	{
+	    lat *= -1;
+       	}
+		
+	dataStore->lat = lat;
+    }
+	
+    ////////////////////////
+    //				      //
+    //  Extract Longitude //
+    //				      //
+    ////////////////////////
+	
+    // extract lon val
+    tokenize(token, sentence, ",");
+	
+    if (NONEMPTY) {
+	// convert lon to float
+	lon = strtof(token, NULL);
+		
+	// extract E/W val
+	tokenize(token, sentence, ",");
+		
+	// if West, make lon negative
+	if (toupper(token) == 'W') 
+	{
+	    lon *= -1;
+	}
+		
+	dataStore->lon = lon;
+    }
+	
+    ////////////////////////////////
+    //					          //
+    //  Extract Speed over ground //
+    //				   	          //
+    ////////////////////////////////    
+	
+    // extract speed
+    tokenize(token, sentence, ",");
+	
+    //  don't care move on...
+ 
+	
+    ///////////////////////////////////
+    //					             //
+    //  Extract angle in degrees     //
+    //					             //
+    ///////////////////////////////////
+	
+    // extract angle
+    tokenize(token, sentence, ",");
+	
+    // don't care move on...
+	
+    ///////////////////////////////////
+    //					             //
+    //  Extract date                 //
+    //					             //
+    ///////////////////////////////////
+	
+    // extract date
+    tokenize(token, sentence, ",");
+	
+    // don't care move on...
+	
+    ///////////////////////////////////
+    //					             //
+    //  Extract magnetic variation   //
+    //					             //
+    ///////////////////////////////////
+	
+    // extract magnetic variation
+    tokenize(token, sentence, ",");
+	
+    // don't care ending...
+	
+	
+    return 0;
+}
