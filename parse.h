303bb904ca11603c9868da5fed20bb5b936b5bc8
Elliot Robinson <elliot.robinson@argiopetech.com>
2011-09-11 22:06:31 -0400

    Moves function declaration to parse.c

--- parse.h
+++ parse.h
@@ -21,25 +21,6 @@ int parseRMC(struct NMEAData*, char*);
 
 int parseZDA(struct NMEAData*, char*);
 
-void convertLatLong(char* dest, float loc) {
-    char temp[12];
-
-    // pull out integer digits, these translate directly
-    sprintf(temp, "%3.0f", loc);
-    dest[0] = temp[0]; // d
-    dest[1] = temp[1]; // d
-    dest[2] = temp[2]; // d
-    
-    dest[3] = 'o';     // o
-    
-    loc = loc - (int) loc; // cut out integer portion
-    loc *= 60;
-    sprintf(temp, "%f", loc);
-    dest[4] = temp[0]; // m
-    dest[5] = temp[1]; // m
-    dest[6] = '.';     // .
-    dest[7] = temp[3]; // s
-    dest[8] = temp[4]; // s
-}
+void convertLatLong(char* dest, float loc);
 
 #endif
